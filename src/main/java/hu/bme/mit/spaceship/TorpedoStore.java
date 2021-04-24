78f102a122e26acff212204f36cd3997b4cc4396
akos989 <morvaiakos1998@gmail.com>
2021-04-24 23:01:31 +0200

    fix - use -= operator instead of =-

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -41,12 +41,9 @@ public class TorpedoStore {
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount =- numberOfTorpedos;
+      this.torpedoCount -= numberOfTorpedos;
       success = true;
-    } else {
-      // simulated failure
-      success = false;
-    }
+    }  // simulated failure
 
     return success;
   }
